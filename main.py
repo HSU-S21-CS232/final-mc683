1294dc33d93374fd28c68023de4376bc6e9f6b0b
Mikey <shpoopdy@gmail.com>
2021-04-21 09:48:49 -0700

    Added auth keys and status update.

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import os
 import tweepy
 from config import consumer_key, consumer_secret, access_key, access_secret
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+
+api = tweepy.API(auth)
+api.update_status('tweet test.')
