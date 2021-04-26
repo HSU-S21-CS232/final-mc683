8c0273ff1d6ee97db2a720261a8048210e158f7d
Mikey <shpoopdy@gmail.com>
2021-04-26 10:20:38 -0700

    Added some more allowed games for variety.

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import tweepy
+import random
 from config import consumer_key, consumer_secret, access_key, access_secret
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -6,10 +7,11 @@ auth.set_access_token(access_key, access_secret)
 
 api = tweepy.API(auth)
 
-allowed_games = "ElderScrolls"
+allowed_games = ["ElderScrolls", "Warcraft", "DeadByBHVR", "SeaOfThieves", "FallGuysGame", "Outriders",
+                "KineticGame", "ConcernedApe"]
 tweet_numb = 1
 
-tweets = tweepy.Cursor(api.user_timeline, allowed_games).items(tweet_numb)
+tweets = tweepy.Cursor(api.user_timeline, random.choice(allowed_games)).items(tweet_numb)
 
 def retweet_bot():
     for tweet in tweets:
