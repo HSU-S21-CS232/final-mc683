7a75a1f47610bfd951be88e3168265ae3a0a8b87
Mikey <shpoopdy@gmail.com>
2021-04-26 10:05:09 -0700

    Commented out excel part.

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 import gspread
 import time
 import tweepy
-from config import consumer_key, consumer_secret, access_key, access_secret
+from functions import retweet_bot
+
 
 """ If you do not have a config.py be sure to run setup first """
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-
+"""
+    This commented out part grabs our premade tweets from an
+    excel sheet and tweets them and then removes them from the sheet.
 
 gc = gspread.service_account('credentials.json')
 
@@ -19,25 +20,12 @@ wks = gc.open("nelja-chirps").sheet1
 # Example, wks.update('A1', [[1, 2], [3, 4]])
 next_chirp = wks.acell('A2').value
 
-# This is the portion that sends the tweet with the given time period beneath.
-api = tweepy.API(auth)
 #api.update_status(next_chirp)
-time.sleep(2)
+
 
 # This deletes the tweet that goes out from your spreadsheet.
-#wks.delete_rows(2)
+wks.delete_rows(2)
+"""
 
-allowed_games = "ElderScrolls"
-tweet_numb = 1
-
-tweets = tweepy.Cursor(api.user_timeline, allowed_games).items(tweet_numb)
-
-def retweet_bot():
-    for tweet in tweets:
-        try:
-            tweet.retweet()
-            print("Retweet Success.")
-        except tweepy.TweepError as e:
-            print(e.reason)
-
+time.sleep(2)
 retweet_bot()
