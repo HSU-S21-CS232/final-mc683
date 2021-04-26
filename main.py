1fb9049087be2b334b5f64fcef6369ec39cfb3ef
Mikey <shpoopdy@gmail.com>
2021-04-26 09:47:51 -0700

    Added retweeting from "Allowed" users.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from config import consumer_key, consumer_secret, access_key, access_secret
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 
+
 gc = gspread.service_account('credentials.json')
 
 # Open a sheet from a spreadsheet in one go
@@ -18,10 +19,25 @@ wks = gc.open("nelja-chirps").sheet1
 # Example, wks.update('A1', [[1, 2], [3, 4]])
 next_chirp = wks.acell('A2').value
 
-
+# This is the portion that sends the tweet with the given time period beneath.
 api = tweepy.API(auth)
-api.update_status(next_chirp)
-time.sleep(5)
+#api.update_status(next_chirp)
+time.sleep(2)
 
 # This deletes the tweet that goes out from your spreadsheet.
-wks.delete_rows(2)
+#wks.delete_rows(2)
+
+allowed_games = "ElderScrolls"
+tweet_numb = 1
+
+tweets = tweepy.Cursor(api.user_timeline, allowed_games).items(tweet_numb)
+
+def retweet_bot():
+    for tweet in tweets:
+        try:
+            tweet.retweet()
+            print("Retweet Success.")
+        except tweepy.TweepError as e:
+            print(e.reason)
+
+retweet_bot()
