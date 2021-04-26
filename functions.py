9fa1c5cbf71f20490cb364016e3bed337825374e
Mikey <shpoopdy@gmail.com>
2021-04-26 10:51:28 -0700

    Created normal_tweet.

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import tweepy
 import random
+import os
+import gspread
 from config import consumer_key, consumer_secret, access_key, access_secret
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -20,3 +22,17 @@ def retweet_bot():
             print("Retweet Success.")
         except tweepy.TweepError as e:
             print(e.reason)
+
+def normal_tweet():
+    gc = gspread.service_account('credentials.json')
+
+    # Open a sheet from a spreadsheet in one go. wks means worksheet.
+    wks = gc.open("nelja-chirps").sheet1
+
+    # Update a range of cells using the top left corner address
+    # Example, wks.update('A1', [[1, 2], [3, 4]])
+    next_chirp = wks.acell('A2').value
+    api.update_status(next_chirp)
+
+    # This deletes the tweet that goes out from your spreadsheet.
+    wks.delete_rows(2)
