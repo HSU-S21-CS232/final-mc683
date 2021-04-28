9db38c3b1585f43eec970461569bfab8e36c1a0f
Mikey <shpoopdy@gmail.com>
2021-04-27 20:34:36 -0700

    Switched function called, imported tweet_page.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import gspread
 import time
 import tweepy
-from functions import retweet_bot, normal_tweet
+from functions import retweet_bot, normal_tweet, tweet_page
 
 
 """ If you do not have a config.py be sure to run setup first """
 
+
 time.sleep(2)
-normal_tweet()
+tweet_page()
