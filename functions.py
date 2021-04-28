85fce8fc4310215c8dcff5cf9a29b264433462be
Mikey <shpoopdy@gmail.com>
2021-04-27 21:24:30 -0700

    Created tweet_page function.

--- functions.py
+++ functions.py
@@ -39,8 +39,25 @@ def normal_tweet():
     # This deletes the tweet that goes out from your spreadsheet.
     wks.delete_rows(2)
 
+FILE_NAME = 'last_seen.txt'
+
+def read_last_seen(FILE_NAME):
+    file_read = open(FILE_NAME, 'r')
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    return last_seen_id
+
+
+def store_last_seen(FILE_NAME, last_seen_id):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return
+
+
 def tweet_page():
-    tweets = api.mentions_timeline()
-    for tweet in tweets:
-        if '#warcraft' in tweet.text.lower():
-            print('twitter.com/Warcraft')
+    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')
+    for tweet in reversed(tweets):
+        if '#warcraft' in tweet.full_text.lower():
+            api.update_status('@' + tweet.user.screen_name + ' Warcrafts page is twitter.com/Warcraft.', tweet.id)
+            store_last_seen(FILE_NAME, tweet.id)
