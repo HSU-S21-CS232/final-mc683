7c654d9105423b0bf546f7475643f6dfe27fddd8
Mikey <shpoopdy@gmail.com>
2021-05-05 08:50:51 -0700

    Moved allowed games here.

---
+++ reg_functions/game_twitter.py
@@ -0,0 +1,3 @@
+allowed_games = ["ElderScrolls", "Warcraft", "DeadByBHVR", "SeaOfThieves", "FallGuysGame", "Outriders",
+                "KineticGame", "ConcernedApe", "DyingLightGame", "DarkSoulsGame", "BloodborneGame", "DarkestDungeon",
+                "RE_Games"]
